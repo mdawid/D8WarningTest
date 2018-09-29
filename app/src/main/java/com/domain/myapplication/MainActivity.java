e7899d2511a593d841fb680ece2d9c13b0c67c86
Marcin Dawid <mdawid@gmail.com>
2018-09-29 14:07:18 +0200

    RxJava network calls and lambdas from Java 8

--- app/src/main/java/com/domain/myapplication/MainActivity.java
+++ app/src/main/java/com/domain/myapplication/MainActivity.java
@@ -1,14 +1,18 @@
 package com.domain.myapplication;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import io.reactivex.Single;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.schedulers.Schedulers;
 import org.springframework.web.client.RestTemplate;
 
 public class MainActivity extends AppCompatActivity {
-	private RestTemplate restTemplate = RestServiceFactory.makeService();
-	private TextView     resultText;
+	private RestTemplate        restTemplate = RestServiceFactory.makeService();
+	private TextView            resultText;
+	private CompositeDisposable compositeDisposable;
 
 	@Override
 	protected void onCreate( Bundle savedInstanceState ) {
@@ -16,22 +20,23 @@ public class MainActivity extends AppCompatActivity {
 		setContentView( R.layout.activity_main );
 
 		resultText = findViewById( R.id.resultText );
+
+		compositeDisposable = new CompositeDisposable();
 	}
 
 	@Override protected void onResume() {
 		super.onResume();
-		new AsyncTask<Void, Void, String>() {
-			@Override protected void onPreExecute() {
-				resultText.setText( "Please wait..." );
-			}
+		compositeDisposable
+				.add( Single.fromCallable( () -> restTemplate.getForObject( "https://api.github.com/repos/square/okhttp/branches", String.class ) )
+							  .doOnSubscribe( disposable -> resultText.setText( "Please wait..." ) )
+							  .subscribeOn( Schedulers.newThread() )
+							  .observeOn( AndroidSchedulers.mainThread() )
+							  .subscribe( s -> resultText.setText( s ),
+										  throwable -> resultText.setText( throwable.getMessage() ) ) );
+	}
 
-			@Override protected String doInBackground( final Void... voids ) {
-				return restTemplate.getForObject( "https://api.github.com/repos/square/okhttp/branches", String.class );
-			}
-
-			@Override protected void onPostExecute( final String bufferooResponse ) {
-				resultText.setText( bufferooResponse );
-			}
-		}.execute();
+	@Override protected void onDestroy() {
+		super.onDestroy();
+		compositeDisposable.dispose();
 	}
 }
